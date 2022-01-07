d61f5837f94f71ece1e033511bd5cfaf91e730c8
Hector <hectorbennett@outlook.com>
2022-01-07 16:04:34 +0000

    rollup

---
+++ dist/cjs/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var core = require('../../../../../../../core');
+
+
+
+Object.keys(core).forEach(function (k) {
+	if (k !== 'default' && !exports.hasOwnProperty(k)) Object.defineProperty(exports, k, {
+		enumerable: true,
+		get: function () { return core[k]; }
+	});
+});
+//# sourceMappingURL=index.js.map
