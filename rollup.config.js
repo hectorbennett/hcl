69bc825249887ece529b12c70f3954fcada9fa54
Hector <hectorbennett@outlook.com>
2022-01-21 08:32:01 +0000

    hcl

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
 
 export default [
   {
-    input: "src/index.js",
+    input: "src/index.ts",
     output: [
       {
         file: packageJson.main,
@@ -18,6 +20,15 @@ export default [
         sourcemap: true,
       },
     ],
-    plugins: [resolve(), commonjs()],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
   },
 ];
