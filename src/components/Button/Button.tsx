40eb920049fff9043daf8f15529df7be4f77f64c
Hector <hectorbennett@outlook.com>
2022-02-23 11:00:27 +0000

    button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,11 +5,20 @@ export interface ButtonProps {
   disabled?: boolean;
   onClick?: () => void;
   children?: ReactNode;
+  backgroundColor?: string;
 }
 
 export const Button = (props: ButtonProps) => {
   return (
-    <button type="button" className={styles.button} disabled={props.disabled}>
+    <button
+      type="button"
+      className={styles.button}
+      disabled={props.disabled}
+      style={{
+        backgroundColor: props.backgroundColor,
+        borderColor: props.backgroundColor,
+      }}
+    >
       {props.children}
     </button>
   );
